5a17b1cf3d1b4915b2dd5945e334e1ccc671c1a3
JQB <jordanqbehn@gmail.com>
2020-10-17 10:05:32 -0500

    updated writePassword(), now prompts user when button is pressed

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+
+
 function writePassword() {
-    var len = Math.floor(Number(prompt("How long?")));
+    var len = Math.floor(Number(prompt("How many characters long would you like your password to be? (must be at least 8)")));
+    var lower = confirm("Include lower case letters?");
+    var upper = confirm("Include upper case letters?");
+    var nums = confirm("Include numbers");
+    var specials = confirm("Include special characters?");
     //if (isNaN) {}
 
     console.log(typeof(len))
