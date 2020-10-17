a384c0911c3b1bec22ce4c46eaaddb8008daa100
JQB <jordanqbehn@gmail.com>
2020-10-17 11:56:04 -0500

    added alerts confirming your criteria choices. choices now update charPool

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerLetters = ["abcdefghijklmnopqrstuvwxyz"]
-var upperLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ "]
-var numberChars = ["1234567890"]
-var specialChars = ["!\"#$%&\'()*+,-.\\/:;<=>?@[]^_`{|}~"]
+var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChars = "1234567890";
+var specialChars = "!\"#$%&\'()*+,-.\\/:;<=>?@[]^_`{|}~";
 console.log(specialChars)
 
 // Write password to the #password input
 
 function writePassword() {
-    var charPool = []
+    var charPool = "";
     var len = Math.floor(Number(prompt("How many characters long would you like your password to be? (must be at least 8)")));
     if (isNaN(len) || len > 128 || len < 8) {
-        alert("Password length must be a number between 8 and 128")
+        alert("Password length must be a number between 8 and 128");
         return
     }
 
-    var lower = confirm("Include lower case letters?");
-    var upper = confirm("Include upper case letters?");
-    var nums = confirm("Include numbers");
-    var specials = confirm("Include special characters?");
+    if (confirm("Include lower case letters?")) {
+        alert("Your password will include lowercase letters");
+        charPool = charPool + lowerLetters;
+    } else { alert("Your password will not include lowercase letters") }
+    if (confirm("Include uppercase letters?")) {
+        alert("Your password will include uppercase letters")
+        charPool = charPool + upperLetters;
+    } else { alert("Your password will not include uppercase letters") }
+    if (confirm("Include numbers")) {
+        alert("Your password will include numbers")
+        charPool = charPool + numberChars;
+    } else { alert("Your password will not include numbers") }
+    if (confirm("Include special characters?")) {
+        alert("Your password will include special characters")
+        charPool = charPool + specialChars;
+    } else { alert("Your password will not include special characters") }
 
-    consol.log(charPool)
+    console.log(charPool);
 
     // var password = generatePassword();
     var passwordText = document.querySelector("#password");
