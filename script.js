974ab68d58f2bf070dd8e9d769f207ffc0e19302
JQB <jordanqbehn@gmail.com>
2020-10-17 10:49:15 -0500

    assigned arrays of characters of each type to variables

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerLetters = ["abcdefghijklmnopqrstuvwxyz"]
+var upperLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ "]
+var numberChars = ["1234567890"]
+var specialChars = ["!\"#$%&\'()*+,-.\\/:;<=>?@[]^_`{|}~"]
+console.log(specialChars)
 
 // Write password to the #password input
 
-
-
 function writePassword() {
+    var charPool = []
     var len = Math.floor(Number(prompt("How many characters long would you like your password to be? (must be at least 8)")));
+    if (isNaN(len) || len > 128 || len < 8) {
+        alert("Password length must be a number between 8 and 128")
+        return
+    }
+
     var lower = confirm("Include lower case letters?");
     var upper = confirm("Include upper case letters?");
     var nums = confirm("Include numbers");
     var specials = confirm("Include special characters?");
-    //if (isNaN) {}
 
-    console.log(typeof(len))
-    console.log(len)
-
+    consol.log(charPool)
 
     // var password = generatePassword();
     var passwordText = document.querySelector("#password");
